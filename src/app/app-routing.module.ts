9c70cb38f58b2bd8184fe2918fbcee023f49b12b
HossamSalaheldeen <hossam.salah1995@gmail.com>
2021-04-19 15:34:15 +0200

    add more 1 route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes:Routes = [
     component: HomeComponent
   },
   {
+    path: 'Angular_Ecommerce',
+    redirectTo: ''
+  },
+  {
     path: 'about',
     component: AboutComponent
   },
